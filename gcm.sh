0efa5cd78b465e40f0879cdbb42d728aafbf90e2
Javi <javi@jpo.cc>
2013-09-23 14:15:41 +0200

    Create gcm.sh

---
+++ gcm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $# = 1 ]; then
+  echo "git add ."
+  git add .
+  echo "git commit -a -m $1"
+  git commit -a -m "$1"
+  echo "Branches"
+  git branch
+
+else
+  echo "Sintaxis: gcm \"Comentario commit git\""
+fi
