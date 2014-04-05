7519df2693e7df31b54ab153be8db032ce958fa2
root <root@mx.de.ocserver.net>
2014-04-06 00:21:16 +0200

    add date switch

--- dump.sh
+++ dump.sh
@@ -4,10 +4,16 @@ source /root/.mysqluser
 # USER
 # PASS
 # BACKUPPATH 
-# ZIP (y/n) #(whether to bzip the databases or not()
+# ZIP (y/n) # whether to bzip the databases or not
+# DATE (y/n) # whether to append the date at the end or not
 DATABASES="$(mysql --user=$USER --password=$PASS -Bse 'show databases')"
 #echo $DATABASES
 cd $BACKUPPATH
+if [ "$DATE" == "y" ]; then
+  APPENDDATE="-"$(date +%m-%d-%y)
+else
+  APPENDDATE=""
+fi
 for db in ${DATABASES[@]}
 do
   if [ $db == "information_schema" ]
@@ -16,9 +22,9 @@ do
   fi
 #  echo ${db}-$(date +%m-%d-%y).sql.bz2 is being saved in $BACKUPPATH 
   if [ "$ZIP" == "y" ];then
-    mysqldump --user=$USER --password=$PASS $db --single-transaction -R | bzip2 -c > ${db}-$(date +%m-%d-%y).sql.bz2
+    mysqldump --user=$USER --password=$PASS $db --single-transaction -R | bzip2 -c > ${db}.sql.bz2
   else
-    mysqldump --user=$USER --password=$PASS $db --single-transaction -R > ${db}-$(date +%m-%d-%y).sql
+    mysqldump --user=$USER --password=$PASS $db --single-transaction -R > ${db}.sql
   fi
 done
 
