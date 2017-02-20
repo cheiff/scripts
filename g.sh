9de9a06f6632c86ea5566c79334a2aaf1d164bbd
Javi <javier@deevop.com>
2017-02-20 22:05:04 +0100

    Added recursive g

--- g.sh
+++ g.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "Searching fuel/app/classes"
-grep -R $1 fuel/app/classes/
+echo "#Searching fuel/app/classes"
+grep -R $2 $1 fuel/app/classes/ |grep -v swp
 echo "##################"
-echo "Searching fuel/app/tasks"
-grep -R $1 fuel/app/tasks/
+echo "#Searching fuel/app/tasks"
+grep -R $2 $1 fuel/app/tasks/ |grep -v swp
 echo "##################"
-echo "Searching fuel/app/views"
-grep -R $1 fuel/app/views/
+echo "#Searching fuel/app/views"
+grep -R $2 $1 fuel/app/views/ |grep -v swp
 echo "##################"
-echo "Searching fuel/packages"
-grep -R $1 fuel/app/packages/
+echo "#Searching fuel/packages"
+grep -R $2 $1 fuel/packages/ |grep -v vendor |grep -v swp
 echo "##################"
 
